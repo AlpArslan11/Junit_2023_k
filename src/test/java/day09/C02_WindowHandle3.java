70717fb6e75de03e6ced968ae443a4274c799d55
Alparslan <maslan11alp@gmail.com>
2023-08-08 03:07:55 +0300

    08082023-1

--- src/test/java/day09/C02_WindowHandle3.java
+++ src/test/java/day09/C02_WindowHandle3.java
@@ -1,8 +1,48 @@
 package day09;
 
-public class C02_WindowHandle3 {
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.WindowType;
+import utilities1.TestBase1;
+
+public class C02_WindowHandle3 extends TestBase1 {
+
+    /*
+    Selenium 4 ile gelmiştir.
+     */
+
+    @Test
+    public void newWindowHandleTest(){
+        // 1. TechPro education sayfasinin titleinin "Techpro Education" metnini icerdigini test edin
+        driver.get("https://www.techproeducation.com");
+        System.out.println(driver.getTitle());
+        String techproWindowHandle = driver.getWindowHandle();
+        Assert.assertTrue(driver.getTitle().contains("TechPro Education"));
+
+        // 2. Yeni bir pencerede Amazon sayfasini acip titleinin "Amazon" metnini icerdigini test edin
+         waitFor(3);
+         driver.switchTo().newWindow(WindowType.WINDOW); // yeni pencere oluşturur ve oraya geçiş yapar
+        driver.get("https://www.amazon.com");
+        System.out.println(driver.getTitle());
+        Assert.assertTrue(driver.getTitle().contains("Amazon"));
 
 
+        // 3.  Yeni bir pencerede linkedin sayfasini acip titleinin "LinkedIn" metnini  icerdigini test edin
+        waitFor(3);
+        driver.switchTo().newWindow(WindowType.WINDOW);
+        driver.get("https://www.linkedin.com");
+        waitFor(3);
+        System.out.println(driver.getTitle());
+        Assert.assertTrue(driver.getTitle().contains("LinkedIn"));
 
-    //  10. Session Selenium Multiple Windows İ Basic Authentication0501  58.12
+        waitFor(3);
+
+        // techpro sayfasına gitmek için driver.switchTo().window(techproHandle)
+        driver.switchTo().window(techproWindowHandle);
+        waitFor(3);
+        Assert.assertTrue(driver.getTitle().contains("TechPro Education"));
+        waitFor(3);
+
+    }
+
 }
