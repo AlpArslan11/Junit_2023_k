df81408245e3df6a8c8010bcb175a3b7f6aba0d6
Alparslan <maslan11alp@gmail.com>
2023-08-17 02:14:05 +0300

    17082023-3

--- src/test/java/Alparslan/day11/Homework_03.java
+++ src/test/java/Alparslan/day11/Homework_03.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.WindowType;
 import org.openqa.selenium.support.ui.Select;
 import utilities1.TestBase1;
 
@@ -12,13 +13,14 @@ import java.util.Arrays;
 import java.util.List;
 
 import static java.lang.Integer.parseInt;
+import static org.openqa.selenium.support.locators.RelativeLocator.with;
 
 public class Homework_03 extends TestBase1 {
 
 
 
     @Test
-    public void genelTestOdev(){
+    public void genelTestOdev() throws InterruptedException {
         //Test01 :
         //1- amazon gidin
         driver.get("https://www.amazon.com");
@@ -48,7 +50,7 @@ public class Homework_03 extends TestBase1 {
 
         //2. arama kutusuna iphone yazip aratin ve bulunan sonuç sayısını yazdırın
         driver.findElement(By.id("twotabsearchtextbox")).sendKeys("iphone", Keys.ENTER);
-        try {   Thread.sleep(1000); } catch (InterruptedException e) { throw new RuntimeException(e); }
+        try {   Thread.sleep(2000); } catch (InterruptedException e) { throw new RuntimeException(e); }
 
         // bulunan sonuç sayısını yazdırın
         String sonucYazisi = driver.findElement(By.xpath("//*[@class='a-section a-spacing-small a-spacing-top-small']")).getText();
@@ -64,12 +66,56 @@ public class Homework_03 extends TestBase1 {
 
 
         //4. ikinci ürüne relative locater kullanarak tıklayin
+        WebElement ilkUrun = driver.findElement(By.xpath("//*[@data-index='2']"));
+        WebElement ikinciUrun =
+                driver.findElement(with(By.tagName("div")).toRightOf(ilkUrun));
+
+        Thread.sleep(2);
+        ikinciUrun.click();
+
 
         //5. ürünün title'ni ve fiyatını variable’a assign edip ürünü sepete ekleyelim
+            String urunTitle = driver.findElement(By.xpath("//span[@id='productTitle']")).getText();
+            String urunPrice = driver.findElement(By.xpath("//*[@class='a-price aok-align-center reinventPricePriceToPayMargin priceToPay']")).getText();
+
+        // ürünü sepete ekleyelim
+            driver.findElement(By.id("add-to-cart-button")).click();
+
+        //sepette warmı kontrol edelim
+        driver.findElement(By.id("nav-cart-count-container")).click();
+        List<WebElement> activeProductsInCart = driver.findElements(By.xpath("//*[@data-itemtype='active']"));
+
+        String []urunTitleArr = urunTitle.split(" ");
+        String urunTitleIlk3tanesiStr ="";
+
+
+        for (int i = 0; i < 3; i++) {
+            urunTitleIlk3tanesiStr += urunTitleArr[i].toString()+" ";
+        }
+
+        for (WebElement e: activeProductsInCart
+             ) {
+
+            if (e.getText().contains(urunTitleIlk3tanesiStr)){
+                Assert.assertTrue("sepetteki urun eklenen urun degil"
+                        ,e.getText().contains(urunTitleIlk3tanesiStr));
+                break;
+            }
+        }
+
+
         //Test03
         //1. yeni bir sekme açarak amazon anasayfaya gidin
+        driver.switchTo().newWindow(WindowType.TAB);
+        Thread.sleep(2);
+        driver.get("https://www.amazon.com");
+
         //2. dropdown’dan bebek bölümüne secin
+        select.selectByVisibleText("Baby");
+        Thread.sleep(2);
+
         //3. bebek puset aratıp bulundan sonuç sayısını yazdırın
+
         //4. sonuç yazsının puset içerdiğini test edin
         //5-üçüncü ürüne relative locater kullanarak tıklayin
         //6-title ve fiyat bilgilerini assign edelim ve ürünü sepete ekleyin
