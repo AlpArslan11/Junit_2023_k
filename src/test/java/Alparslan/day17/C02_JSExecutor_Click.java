eaa350fe18ca2f74993f67b8efe635d2e831025c
Alparslan <maslan11alp@gmail.com>
2023-08-29 15:16:05 +0300

    29082023-1

--- src/test/java/Alparslan/day17/C02_JSExecutor_Click.java
+++ src/test/java/Alparslan/day17/C02_JSExecutor_Click.java
@@ -6,6 +6,8 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import utilities1.TestBase1;
 
+import java.util.Set;
+
 
 public class C02_JSExecutor_Click extends TestBase1 {
 
@@ -14,10 +16,23 @@ public class C02_JSExecutor_Click extends TestBase1 {
             //    Techpro education ana sayfasina git
             driver.get("https://www.techproeducation.com");
             waitFor(3);
+
+            String actualWindowHandle = driver.getWindowHandle();
             //    LMS LOGIN elementine tikla
-            clickByJS(driver.findElement(By.linkText("LMS LOGIN")));
+            clickByJS(driver.findElement(By.linkText("LMS")));
             //    Cikan sayfayinin URL inin “https://lms.techproeducation.com/” oldugunu test et
             waitFor(3);
+
+            Set<String> allWindowHandles = driver.getWindowHandles();
+            for (String e : allWindowHandles){
+                if (!e.equals(actualWindowHandle)){
+                    driver.switchTo().window(e);
+                    break;
+                }
+            }
+
+
+
             Assert.assertEquals("https://lms.techproeducation.com/",driver.getCurrentUrl());
         }
     }
